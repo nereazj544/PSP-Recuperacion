1175cf88c3b8d29db4e54727eb8e34d678a95bce
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2024-04-11 20:27:08 +0200

    Update Ejercicio6.java

--- src/Abril/Unidad2/Clase/Banco_del_parque/Ejercicio6.java
+++ src/Abril/Unidad2/Clase/Banco_del_parque/Ejercicio6.java
@@ -1,1 +1,21 @@
+package Abril.Unidad2.Clase.Banco_del_parque;
 
+public class Ejercicio6 {
+
+
+
+
+
+
+}
+
+/*
+ * Los metodos "wait", "sleep" los deja en modo de espera
+ * Hay dos:
+ * wait()
+ * wait(ms) (despues de el tiempo de espera, no hace falta usar notify)
+ * relacionados con estos dos metodos: notify (notifica a otro) y notifyAll (notifica a todos) (comunicacion entre hilos).
+ * Notifican el estado en el que tienen que estar. Esta es la unica comunicacion que exite entre hilos.
+ * ! Solo se pueden ejecutar el monitor.
+ * Pueden invocarlo cual quierer objeto, solo se pueden invocar en una sececion critica y con el monitor.
+ */
