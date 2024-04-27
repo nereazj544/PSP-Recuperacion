2849f8791a5bb7d82d29971df142c69d9a591a9c
nereazj544 <nzjvirtual@gmail.com>
2024-04-27 15:51:04 +0200

    no muestra nada

--- src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/Banco.java
+++ src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/Banco.java
@@ -1,37 +1,37 @@
 package ACTIVIDADES_ENTREGA.Banco_Del_Parque;
-import static ACTIVIDADES_ENTREGA.Fumadores.Fork.Main.actualizar;
 
 public class Banco {
     private int plazaslibres;
     private boolean pausado;
 
     public Banco(int plazas) {
-        plazaslibres = plazaslibres;
+        plazaslibres = plazas;
     }
 
-    public synchronized void pausar(){
-        pausado=true;
+    public synchronized void pausar() {
+        pausado = true;
     }
 
-    public synchronized void reanudar(){
-        pausado=false;
+    public synchronized void reanudar() {
+        pausado = false;
         notifyAll();
     }
 
-
-
-    public synchronized void sentarse(){
-        while (plazaslibres == 0) {
-            try {
-                actualizar(Thread.currentThread().getName() + " esperando");
-                wait();
-            } catch (InterruptedException e) {}
-
+    public synchronized void sentarse() {
+        while (plazaslibres == 0 || pausado) {
+            if (pausado) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
             plazaslibres--;
         }
     }
 
-    public synchronized void levantarse(){
+    public synchronized void levantarse() {
         plazaslibres++;
         notify();
     }
--- src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/PersonaTarea.java
+++ src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/PersonaTarea.java
@@ -1,21 +1,13 @@
 package ACTIVIDADES_ENTREGA.Banco_Del_Parque;
 
-import static ACTIVIDADES_ENTREGA.Fumadores.Fork.Main.actualizar;
-
-
-
 public class PersonaTarea extends Thread {
     private Banco b;
-
     private long paseo;
     private long sentado;
-
-    // private JTextArea textArea;
 
     public PersonaTarea(String name, Banco b) {
         super(name);
         this.b = b;
-        // this.textArea = textArea;
         this.paseo = (long) ((Math.random() * 1000000) % 2001 + 100);
         this.sentado = (long) ((Math.random() * 1000000) % 6001 + 100);
     }
@@ -23,16 +15,16 @@ public class PersonaTarea extends Thread {
     @Override
     public void run() {
         try {
-           actualizar(getName() + " llego al banco");
+            Main.actualizar(getName() + " llego al banco");
             Thread.sleep(paseo);
             b.sentarse();
-            actualizar(getName() + " sento al banco");
+            Main.actualizar(getName() + " sento al banco");
             Thread.sleep(sentado);
             b.levantarse();
-            actualizar(getName() + " se ha ido del banco");
+            Main.actualizar(getName() + " se ha ido del banco");
 
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
-
 }
