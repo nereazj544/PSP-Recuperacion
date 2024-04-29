105d70856908ed92c564f4e45779e4c99e268bc2
nereazj544 <nzjvirtual@gmail.com>
2024-04-29 14:23:20 +0200

    Nota mental: REVISAR LOS IMPORT STATIC ANTES DE MANDAR A TOMAR POR CULO A....

--- src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/Main.java
+++ src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/Main.java
@@ -1,5 +1,6 @@
 package ACTIVIDADES_ENTREGA.Banco_Del_Parque;
 
+
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -7,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.concurrent.Semaphore;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,107 +17,105 @@ import javax.swing.SwingUtilities;
 
 public class Main extends JFrame implements WindowListener {
 
-	private static final long serialVersionUID = 1L;
-	private static final JTextArea textArea = new JTextArea();
-	private JButton pausa = new JButton("PAUSA");
-	private JButton reanudar = new JButton("REANUDAR");
+    private static final long serialVersionUID = 1L;
+    private static final JTextArea textArea = new JTextArea();
+    private JButton pausa = new JButton("PAUSA");
+    private JButton reanudar = new JButton("REANUDAR");
 
-	private Banco banco = new Banco(14);
-	private PersonasTarea[] personas = new PersonasTarea[15];
-	private Semaphore semaphore = new Semaphore(14, true);
+    private Banco banco = new Banco(14);
+    private PersonasTarea[] personas = new PersonasTarea[15];
+    private Semaphore semaphore = new Semaphore(14, true);
 
+    public Main() {
+        super("Banco del Parque");
+        this.addWindowListener(this);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        Container contentPane = getContentPane();
+        contentPane.setPreferredSize(new Dimension(900, 700));
+        JPanel panel = new JPanel();
+        pausa.addActionListener(this::pausa);
+        panel.add(pausa, BorderLayout.WEST);
+        reanudar.setEnabled(false);
+        reanudar.addActionListener(this::reanudar);
+        panel.add(reanudar, BorderLayout.EAST);
+        contentPane.add(panel, BorderLayout.NORTH);
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(null);
+    }
 
-	public Main() {
-		super("Fumadores");
-		this.addWindowListener(this);
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		Container contentPane = getContentPane();
-		contentPane.setPreferredSize(new Dimension(900, 700));
-		JPanel panel = new JPanel();
-		pausa.addActionListener(this::pausa);
-		panel.add(pausa, BorderLayout.WEST);
-		reanudar.setEnabled(false);
-		reanudar.addActionListener(this::reanudar);
-		panel.add(reanudar, BorderLayout.EAST);
-		contentPane.add(panel, BorderLayout.NORTH);
-		textArea.setEditable(false);
-		JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		contentPane.add(scrollPane, BorderLayout.CENTER);
-		pack();
-		setLocationRelativeTo(null);
-	}
+    public static void actualizar(String msg) {
+        SwingUtilities.invokeLater(() -> textArea.append(msg));
+    }
 
-	public static void actualizar(String msg) {
-		SwingUtilities.invokeLater(() -> textArea.append(msg));
-	}
+    private void pausa(ActionEvent e) {
+        pausa.setEnabled(false);
+        reanudar.setEnabled(true);
+        textArea.append("PAUSADO\n");
+        for (PersonasTarea persona : personas) {
+            persona.pausar();
+        }
+        
+    }
 
-	private void pausa(ActionEvent e) {
-		pausa.setEnabled(false);
-		reanudar.setEnabled(true);
-		textArea.append("PAUSADO\n");
-		for (PersonasTarea personasTarea : personas) {
-			personasTarea.pausar();
-		}
-		
-	}
+    private void reanudar(ActionEvent e) {
+        pausa.setEnabled(true);
+        reanudar.setEnabled(false);
+        textArea.append("REANUDADO\n");
+        for (PersonasTarea persona : personas) {
+            persona.reanudar();
+        }
+        
+    }
 
-	private void reanudar(ActionEvent e) {
-		pausa.setEnabled(true);
-		reanudar.setEnabled(false);
-		textArea.append("REANUDADO\n");
-		for (PersonasTarea personasTarea : personas) {
-			personasTarea.reanudar();
-		}
+    private void iniciar() {
+        setVisible(true);
 
-		
-	}
+        for (int i = 0; i < personas.length; i++) {
+            personas[i] = new PersonasTarea("Persona " + (i + 1), banco, semaphore);
+            personas[i].start();
+        }
 
-	private void iniciar() {
-		setVisible(true);
+    }
 
-		for (int i = 0; i < personas.length; i++) {
-			personas[i] = new PersonasTarea("Persona " + (i + 1), banco, semaphore);
-			personas[i].start();
-		}
+    private static void crear() {
+        new Main().iniciar();
+    }
 
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::crear);
+    }
 
-	private static void crear() {
-		new Main().iniciar();
-	}
+    @Override
+    public void windowOpened(WindowEvent e) {
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(Main::crear);
-	}
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
 
-	@Override
-	public void windowOpened(WindowEvent e) {
-	}
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
 
-	@Override
-	public void windowClosing(WindowEvent e) {
-		System.exit(0);
-	}
+    @Override
+    public void windowIconified(WindowEvent e) {
+    }
 
-	@Override
-	public void windowClosed(WindowEvent e) {
-	}
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+    }
 
-	@Override
-	public void windowIconified(WindowEvent e) {
-	}
+    @Override
+    public void windowActivated(WindowEvent e) {
+    }
 
-	@Override
-	public void windowDeiconified(WindowEvent e) {
-	}
-
-	@Override
-	public void windowActivated(WindowEvent e) {
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent e) {
-	}
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+    }
 
 }
--- src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/PersonasTarea.java
+++ src/ACTIVIDADES_ENTREGA/Banco_Del_Parque/PersonasTarea.java
@@ -1,26 +1,27 @@
 package ACTIVIDADES_ENTREGA.Banco_Del_Parque;
 
-import static ACTIVIDADES_ENTREGA.Fumadores.Fork.Main.actualizar;
+import static ACTIVIDADES_ENTREGA.Banco_Del_Parque.Main.actualizar;
 
 import java.util.concurrent.Semaphore;
 
 public class PersonasTarea extends Thread {
     private Banco banco;
     private Semaphore semaphore;
-    private boolean p = false;
+    private volatile boolean pausado;
 
     public PersonasTarea(String name, Banco banco, Semaphore semaphore) {
         super(name);
         this.banco = banco;
         this.semaphore = semaphore;
+        this.pausado = false;
     }
 
     public synchronized void pausar() {
-        p = true;
+        this.pausado = true;
     }
 
     public synchronized void reanudar() {
-        p = false;
+        this.pausado = false;
         notify();
     }
 
@@ -28,33 +29,35 @@ public class PersonasTarea extends Thread {
     public void run() {
         while (!isInterrupted()) {
             synchronized (this) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    return;
+                while (pausado) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
                 }
             }
-        }
 
-        try {
-            semaphore.acquire();
+            try {
+                semaphore.acquire();
 
-            if (banco.ocupar()) {
-                actualizar(getName() + " ocupo");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    return;
+                if (banco.ocupar()) {
+                    actualizar(getName() + " ocupó una plaza\n");
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    banco.liberar();
+                    actualizar(getName() + " liberó la plaza\n");
+                } else {
+                    actualizar(getName() + " no pudo ocupar una plaza\n");
                 }
-
-                banco.liberar();
-                actualizar(getName() + " ha liberado");
-            } else {
-                actualizar(getName() + " no pudo ocupar una plaza");
+            } catch (InterruptedException e) {
+                return;
+            } finally {
+                semaphore.release();
             }
-        } catch (Exception e) {
-            return;
         }
-        semaphore.release();
     }
 }
