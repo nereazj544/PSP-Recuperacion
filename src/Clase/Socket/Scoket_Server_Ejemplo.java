6b44df9044591f78066e138451e4491abd68415e
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2024-04-29 21:30:06 +0200

    Add files via upload

--- src/Clase/Socket/Scoket_Server_Ejemplo.java
+++ src/Clase/Socket/Scoket_Server_Ejemplo.java
@@ -1,29 +1,28 @@
-package Clase.Socket;
+package Clase.Scoket;
 
-import java.io.BufferedReader;
+// import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+// import java.io.InputStreamReader;
+// import java.io.OutputStreamWriter;
+// import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.util.concurrent.Executor;
+// import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.net.*;
 
 public class Scoket_Server_Ejemplo {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5000); // ! Siempre se va a poner la escucha en este puerto
-        System.out.println("COMUNICANDOSE POR EL PUERTO: 5000");
-        
-        ExecutorService e = Executors.newFixedThreadPool(100);
-        
-        while (true) {
-            // ! Creamos el hilo en el servidor ⬇️
-            Socket socket = serverSocket.accept();
-            e.execute(new TareaRespuesta(socket));
-
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            System.out.println("COMUNICANDOSE POR EL PUERTO: 5000");
             
+            ExecutorService e = Executors.newFixedThreadPool(100);
+            
+            while (true) {
+                // ! Creamos el hilo en el servidor ⬇️
+                Socket socket = serverSocket.accept();
+                e.execute(new TareaRespuesta(socket));
+            }
         }
 
     }
