8707f7197fa558c7375b0c600948eaa985a7f391
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2024-05-06 19:34:46 +0200

    Add files via upload

--- src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/Server/Conexion.java
+++ src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/Server/Conexion.java
@@ -3,15 +3,21 @@ package Clase.Scoket.Modificacion_serv_echo.Server;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 
-public class Conexion{
+public class Conexion {
     private Socket socket;
     private Almacen Almacen;
-
+    private Emisor emisor;
+    private Recpetor recpetor;
 
     public Conexion(Socket socket, ExecutorService service) {
         this.socket = socket;
     }
 
+    public void Finalizar() {
+        //! El repcetor es el que finalizara la conexion
 
+        //TODO: EL RECPTROR TIENE QUE INVOCAR ESTE METODO
+
+    }
 
 }
