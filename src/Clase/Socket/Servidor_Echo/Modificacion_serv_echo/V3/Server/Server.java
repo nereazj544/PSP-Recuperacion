5011cfcaeadd3d776c65264e241665d3b86470cb
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2024-05-09 21:04:00 +0200

    Add files via upload

--- src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/V3/Server/Server.java
+++ src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/V3/Server/Server.java
@@ -3,7 +3,6 @@ package Clase.Scoket.Modificacion_serv_echo.V3.Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,16 +11,17 @@ import java.util.concurrent.Executors;
  */
 public class Server {
 	public static void main(String[] args) throws IOException {
-		ServerSocket serverSocket = new ServerSocket(5000);
-		ExecutorService executor = Executors.newFixedThreadPool(100);
-		Almacen a = new Almacen();
-		while (true) {
-			Socket socket = serverSocket.accept();
-			Recpetor r = new Recpetor(socket, a);
-			Emisor e = new Emisor(socket, a);
-			executor.execute(r);
-			executor.execute(e);
+		try (ServerSocket serverSocket = new ServerSocket(5000)) {
+			ExecutorService executor = Executors.newFixedThreadPool(100);
+			Almacen a = new Almacen();
+			while (true) {
+				Socket socket = serverSocket.accept();
+				Recpetor r = new Recpetor(socket, a);
+				Emisor e = new Emisor(socket, a);
+				executor.execute(r);
+				executor.execute(e);
 
+			}
 		}
 	}
 
