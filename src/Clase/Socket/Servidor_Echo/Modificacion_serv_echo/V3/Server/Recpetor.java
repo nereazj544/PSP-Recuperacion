72bdde88fc93f199c72b17d563c9e550665a23e8
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2024-05-09 20:39:31 +0200

    Add files via upload

---
+++ src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/V3/Server/Recpetor.java
@@ -0,0 +1,20 @@
+package Clase.Scoket.Modificacion_serv_echo.V3.Server;
+
+import java.net.Socket;
+
+
+public class Recpetor implements Runnable {
+    private Socket socket;
+    
+
+    public Recpetor(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
--- src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/V3/Server/Server.java
+++ src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/V3/Server/Server.java
@@ -1,1 +1,27 @@
+package Clase.Scoket.Modificacion_serv_echo.V3.Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Server
+ */
+public class Server {
+    public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = new ServerSocket(5000);
+		ExecutorService executor = Executors.newFixedThreadPool(100);
+		while (true) {
+			Socket socket = serverSocket.accept();
+			Recpetor r = new Recpetor(socket);
+			Emisor e = new Emisor(socket);
+			executor.execute(r);
+			executor.execute(e);
+		}
+	}
+
+    
+}
