ae7967b117cfa3b1414de9894d137d870a7df7ed
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2024-05-13 18:49:06 +0200

    Add files via upload

--- src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/V4/Server/Recpetor.java
+++ src/Clase/Socket/Servidor_Echo/Modificacion_serv_echo/V4/Server/Recpetor.java
@@ -1,10 +1,13 @@
 package Clase.Scoket.Modificacion_serv_echo.V4.Server;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+
+import javax.xml.crypto.Data;
 
 public class Recpetor implements Runnable {
     private Socket socket;
@@ -18,8 +21,13 @@ public class Recpetor implements Runnable {
 
     @Override
     public void run() {
-        try (Socket socket = this.socket ) {
-            
+        try (DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream()) ) {
+            Boolean fin = false;
+
+            while (!fin) {
+                
+            }
         } catch (Exception e) {
             // TODO: handle exception
         }
